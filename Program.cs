d1fc6c7ddc99ca8f52026976f7fffb8584a27e33
Katlego Makhuba <128791831+st10366193@users.noreply.github.com>
2025-03-18 10:31:56 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auto_playsound
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // creatieng a instance for class 
+            new auto_play_sound() { };
+
+        }
+    }
+}
---
+++ auto_play_sound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+
+namespace auto_playsound
+{
+    public class auto_play_sound
+    {
+        // constructor 
+        public auto_play_sound()
+        {
+            // getting the app full location
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+            
+            // replace bin\debug\
+            string new_path = full_location.Replace("bin\\Debug\\","");
+
+            // combne the paths
+            string full_path = Path.Combine(new_path, "greeting.wav");
+
+            // try and catch to play the audio
+            try
+            {
+                //making use of using functions
+                using (SoundPlayer play = new SoundPlayer (full_path))  
+                {
+                    // play the file 
+                    play.PlaySync();
+                }//end of using functions
+            }
+            catch (Exception error) { Console.WriteLine(error.Message);}// end of catch
+
+        }// end of constructor
+    }// end of class 
+}//end of namespace
